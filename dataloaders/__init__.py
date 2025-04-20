a3271bf2ad0c63be5bda062381f9228d24612617
Pavel Shapka <shapka.pa@phystech.edu>
2025-04-20 18:50:42 +0300

    11

--- dataloaders/__init__.py
+++
@@ -1,7 +0,0 @@
-from ..classification.default_loader import get_base_dataloaders, CIFAR10_MEANS, CIFAR10_STD
-from ..regression.sas_stack.sars_dataloader import get_sars_dataloaders
-
-__all__ = ["get_base_dataloaders",
-           "get_sars_dataloaders",
-           "CIFAR10_MEANS",
-           "CIFAR10_STD"]
--- sas_stack/sars_dataloader.py
+++ sas_stack/sars_dataloader.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
+import jax.numpy as jnp
 
-import math
-
-from ...classification.default_loader import CIFAR10_MEANS, CIFAR10_STD
+CIFAR10_MEANS = jnp.array([0.4914, 0.4822, 0.4465])  # CIFAR-10
+CIFAR10_STD = jnp.array([0.229, 0.224, 0.225])
 
 def normalize(image, label):
     image = tf.cast(image, tf.float32) / 255.0 
@@ -20,35 +20,34 @@ def generate_trajectory(image,
     """Generate a SARS trajectory from a noise to an image"""
 
     z = tf.random.normal(tf.shape(image), dtype=image.dtype)
-
-    ts = math.sqrt(2) * tf.range(num_steps, dtype=tf.float32) % 1.0
-    # ts = tf.linspace(0.0, 1.0, num_steps)
+    ts = tf.linspace(0.0, 1.0, num_steps)
     ts = tf.reshape(ts, (num_steps, 1, 1, 1))
 
     trajectory = z * (1 - ts) + image * ts # [None, 32, 32, 3] * [num_steps, None, None, None]
 
     s = trajectory[:-1]     # [num_steps-1, 32, 32, 3]
-    a = trajectory[1:] - s  # [num_steps-1, 32, 32, 3]
+    s_hat = trajectory[1:]  # [num_steps-1, 32, 32, 3]
+    a = s_hat - s           # [num_steps-1, 32, 32, 3]
     rewards = reward * (gamma ** tf.reverse(tf.range(num_steps-1, dtype=tf.float32), axis=[0])) # [num_steps-1]
 
-    transitions = tf.concat([s, a], axis=-1) # [num_steps-1, 2 * 32 * 32]
+    transitions = tf.concat([s, a, s_hat], axis=-1) # [num_steps-1, 4 * 32 * 32]
     
     return transitions, rewards
 
 
-def get_sar_dataloaders(batch_size: int=128,
-                        reward: float=1.,
-                        gamma: float=0.99,
-                        num_steps: int=10):
+def get_sars_dataloaders(batch_size: int=128,
+                         reward: float=1.,
+                         gamma: float=0.99,
+                         num_steps: int=10):
     train_ds = tfds.load('cifar10', split="train", as_supervised=True)
     test_ds = tfds.load('cifar10', split="test", as_supervised=True)
 
     def prepare_ds(ds, train=True):
         ds = ds.map(normalize, num_parallel_calls=tf.data.AUTOTUNE)
         ds = ds.map(lambda image, _: generate_trajectory(image=image,
-                                                         reward=reward,
-                                                         gamma=gamma,
-                                                         num_steps=num_steps),
+                                                     reward=reward,
+                                                     gamma=gamma,
+                                                     num_steps=num_steps),
                     num_parallel_calls=tf.data.AUTOTUNE)
         ds = ds.flat_map(lambda transitions, rewards: tf.data.Dataset.from_tensor_slices((transitions, rewards)))
 
