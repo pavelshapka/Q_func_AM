bb579cd8f4226acd7583c280f82ca5727a4b707e
Pavel Shapka <shapka.pa@phystech.edu>
2025-04-20 20:25:04 +0300

    14

--- sa_stack/__init__.py
+++ sa_stack/__init__.py
@@ -1,5 +1,5 @@
 from .sar_dataloader import get_sar_dataloaders
-from .trainer import Trainer
+from .trainer import TrainerModule
 from .regression_sa_inception import RegressionSAInceptionNetV1
 
 __all__ = ["get_sar_dataloaders", "Trainer", "RegressionSAInceptionNetV1"]
