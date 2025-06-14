cf441babcddc304874842b12936ca0072e800a42
Pavel <shapka.pavel@gmail.com>
2025-06-14 10:54:28 +0000

    1

--- td_sa_stack/config.py
+++ td_sa_stack/config.py
@@ -6,10 +6,11 @@ def get_config():
 
     config.seed = 0
     config.wandb_track = True
-    config.multi_device = False
+    config.multi_device = True
 
     # data
     config.data = data = ml_collections.ConfigDict()
+    data.num_threads = 48
     data.min_traj_len = 40
     data.max_traj_len = 80
     data.gamma = 0.97
@@ -27,12 +28,12 @@ def get_config():
     config.train = train = ml_collections.ConfigDict()
     train.n_steps = 500_000
     train.lr = 1e-4
-    train.batch_size = 128
-    train.ema = 0.99
+    train.batch_size = 1024 # 512 for each device
+    train.ema = 0.995
 
-    train.update_target_every = 30
-    train.log_every = 50
-    train.save_every = 10_000
+    train.update_target_every = 50
+    train.log_every = 200
+    train.save_every = 1_000
 
     # evaluation
     config.eval = eval = ml_collections.ConfigDict()
